33077132def59bd394888491c764fc02d8372aaf
Mordechai Dror <vorant94@gmail.com>
2023-07-07 14:12:42 +0300

    swapped s3 public access with cloudfront distribution

--- lib/vorant94-io-stack.ts
+++ lib/vorant94-io-stack.ts
@@ -1,16 +1,39 @@
 import { Construct } from 'constructs';
 import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
-import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3';
+import {
+  BlockPublicAccess,
+  Bucket,
+  BucketAccessControl,
+  ObjectOwnership,
+} from 'aws-cdk-lib/aws-s3';
+import { Distribution, OriginAccessIdentity } from 'aws-cdk-lib/aws-cloudfront';
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
 
 export class Vorant94IoStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new Bucket(this, `images-bucket`, {
-      publicReadAccess: true,
-      blockPublicAccess: BlockPublicAccess.BLOCK_ACLS,
+    const imagesBucket = new Bucket(this, `images-bucket`, {
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
       removalPolicy: RemovalPolicy.DESTROY,
+      accessControl: BucketAccessControl.PRIVATE,
+      objectOwnership: ObjectOwnership.BUCKET_OWNER_ENFORCED,
       autoDeleteObjects: true,
+    });
+
+    const imagesOriginAccessIdentity = new OriginAccessIdentity(
+      this,
+      'images-origin-access-identity'
+    );
+    imagesBucket.grantRead(imagesOriginAccessIdentity);
+
+    const imagesOrigin = new S3Origin(imagesBucket, {
+      originAccessIdentity: imagesOriginAccessIdentity,
+    });
+    new Distribution(this, 'images-distribution', {
+      defaultBehavior: {
+        origin: imagesOrigin,
+      },
     });
   }
 }
